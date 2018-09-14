53066421428e5b2330adc76f2851f5d907d7a100
Danny Ward <liege88@gmail.com>
2018-09-14 23:06:38 +0800

    added smooth scrolling to app.js. Added media queries. made card columns responsive. Added images to candles and soaps.

--- app.js
+++ app.js
@@ -13,3 +13,39 @@ $("#downButton").click(function() {
         scrollTop: $("#navbarNav").offset().top
     }, 2000);
 });
+
+$("#homeButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#home").offset().top - '85'
+    }, 2000);
+});
+
+$("#introButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#intro").offset().top - '85'
+    }, 2000);
+});
+
+$("#candlesButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#candles").offset().top - '85'
+    }, 2000);
+});
+
+$("#soapsButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#soaps").offset().top - '85'
+    }, 2000);
+});
+
+$("#bathBombsButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#bathBombs").offset().top - '85'
+    }, 2000);
+});
+
+$("#contactUsButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#contactUs").offset().top - '85'
+    }, 2000);
+});
